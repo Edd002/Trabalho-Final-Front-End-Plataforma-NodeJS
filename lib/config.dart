8631a79861eaf233d16b619d45f843237079e859
Edd002 <42621512+Edd002@users.noreply.github.com>
2022-05-21 21:19:07 -0300

    Commit 2 - 21/05/2021

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String appName = "Flutter & NodeJS - CRUD Produto";
-  static const String apiURL = '10.0.2.2:5000'; // 10.0.2.2 é um alias do emulador (AVD) para o localhost
+  static const String apiURL = 'trabalho-final-back-end-nodejs.herokuapp.com'; // Testes locais: alterar para 10.0.2.2:5000 que é um alias do emulador (AVD) para o localhost
   static const securityResgisterAPIuri = "seguranca/register";
   static const securityLoginAPIuri = "seguranca/login";
   static const productsAPIuri = "api/produtos";
